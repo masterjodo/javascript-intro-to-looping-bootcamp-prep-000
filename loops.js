9c2407680be5b53dc70949824404405a3d7bba1d
Jawad Ashour <jawadashour@gmail.com>
2019-01-02 20:03:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 var forLoop = function forloop(array) {
   for (let i = 0; i < 25; i++) {
-    array.push('I am ${i} strange loop.')
+    if (i = 1) {
+      array.push('I am 1 strange loop.')
+    }
+    else {
+      array.push ('I am ${i} strange loops.')
+    }
     return array;
   }
 }
